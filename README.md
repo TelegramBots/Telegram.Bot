12c68444b17045fad2e1f59ea3378b628d486fe7
Robin Müller <robin.mueller@outlook.de>
2015-07-01 12:42:21 +0200

    now we have a readme

---
+++ README.md
@@ -0,0 +1,33 @@
+## Telegram Bot Api Library
+
+C# library to talk to Telegrams Bot API (https://core.telegram.org/bots/api)
+
+## Usage
+
+```C#
+static async void testApiAsync()
+{
+    var Bot = new Telegram.Bot.Api("your API access Token");
+    var me = await Bot.GetMe();
+    System.Console.WriteLine("Hello my name is " + me.FirstName);
+}
+```
+
+```C#
+static void testApi()
+{
+    var Bot = new Telegram.Bot.Api("your API access Token");
+    var me = Bot.GetMe().Result;
+    System.Console.WriteLine("Hello my name is " + me.FirstName);
+}
+```
+
+## Installation
+
+Install as [NuGet package](https://www.nuget.org/packages/Telegram.Bot/):
+
+    Install-Package Telegram.Bot
+    
+## API Coverage
+
+There are functions for all available API methods. (2015-07-01)
--- Telegram.Bot/Properties/AssemblyInfo.cs
+++ Telegram.Bot/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Telegram Bot Api")]
-[assembly: AssemblyDescription("Telegram Bot Api C# Wrapper")]
+[assembly: AssemblyTitle("Telegram Bot API")]
+[assembly: AssemblyDescription("Telegram Bot API C# Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robin Müller")]
-[assembly: AssemblyProduct("Telegram.Bot")]
+[assembly: AssemblyProduct("Telegram Bot API")]
 [assembly: AssemblyCopyright("Copyright © Robin Müller 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
