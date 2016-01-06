96973942b3d8c45b46a1fc817f1c7592ec877ef4
Robin Müller <robin.mueller@outlook.de>
2016-01-06 16:22:29 +0100

    Main project migrated

--- src/Telegram.Bot/Properties/AssemblyInfo.cs
+++ src/Telegram.Bot/Properties/AssemblyInfo.cs
@@ -14,18 +14,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Robin Müller 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.0.1.*")]
-[assembly: AssemblyFileVersion("6.0.1.0")]
-[assembly: AssemblyInformationalVersion("6.0.1.*")]
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42e7cd69-9c5f-4a63-9341-e8c98c7cb8af")]
