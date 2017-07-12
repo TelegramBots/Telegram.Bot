765ba993f87e84eed901a25f8097f002f22d7a8b
Olfi01 <flom@meyer-alpers.de>
2017-07-12 09:24:44 +0200

    Corrected docu for SendChatDescriptionAsync

--- src/Telegram.Bot/TelegramBotClient.cs
+++ src/Telegram.Bot/TelegramBotClient.cs
@@ -1702,7 +1702,7 @@ namespace Telegram.Bot
         /// Use this method to change the description of a supergroup or a channel. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights.
         /// </summary>
         /// <param name="chatId">Unique identifier for the target chat or username of the target channel</param>
-        /// <param name="description">New chat description, 0-255 characters</param>
+        /// <param name="description">New chat description, 0-255 characters. Defaults to an empty string, which would clear the description.</param>
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <returns>Returns true on success.</returns>
         /// <see href="https://core.telegram.org/bots/api#setchatdescription"/>
