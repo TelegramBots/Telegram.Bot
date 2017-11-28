9a4e4d4c14956d5b47a9f80e9a996d2c7e935883
u_m09ad <smbondar@alfabank.ru>
2017-11-28 12:48:45 +0300

    [+] Should_Send_Sticker Test

--- test/Telegram.Bot.Tests.Integ/Stickers/StickersTests.cs
+++ test/Telegram.Bot.Tests.Integ/Stickers/StickersTests.cs
@@ -2,18 +2,24 @@
 using Telegram.Bot.Tests.Integ.Common;
 using Telegram.Bot.Types;
 using Xunit;
+using System.Linq;
+using Telegram.Bot.Types.Enums;
 
 namespace Telegram.Bot.Tests.Integ.Stickers
 {
     [Collection(CommonConstants.TestCollections.Stickers)]
     [TestCaseOrderer(CommonConstants.TestCaseOrderer, CommonConstants.AssemblyName)]
-    public class StickersTests
+    public class StickersTests : IClassFixture<StickersTestsFixture>
     {
+        private readonly StickersTestsFixture _classFixture;
+
         private readonly TestsFixture _fixture;
 
-        public StickersTests(TestsFixture assemblyFixture)
+        public StickersTests(StickersTestsFixture classFixture)
         {
-            _fixture = assemblyFixture;
+            _classFixture = classFixture;
+            _fixture = _classFixture.TestsFixture;
+            _classFixture.ChatId = ConfigurationProvider.TestConfigurations.ChannelChatId;
         }
 
         #region 1. Get and send stickers from a set
@@ -34,16 +40,30 @@ namespace Telegram.Bot.Tests.Integ.Stickers
             Assert.True(20 < stickerSet.Stickers.Count);
             // ToDo: Add more asserts
 
-            // ToDo: Assign sticker set to this class's fixture
+            _classFixture.StickerSet = stickerSet;
+        }
+
+        [Fact(DisplayName = FactTitles.ShouldSendSticker)]
+        [Trait(CommonConstants.MethodTraitName, CommonConstants.TelegramBotApiMethods.SendSticker)]
+        [ExecutionOrder(1.2)]
+        public async Task Should_Send_Sticker()
+        {
+            await _fixture.SendTestCaseNotificationAsync(FactTitles.ShouldSendSticker);
+            FileToSend stickerFile = new FileToSend(_classFixture.StickerSet.Stickers[0].FileId);
+
+            Message message = await _fixture.BotClient.SendStickerAsync(_classFixture.ChatId, stickerFile);
+
+            Assert.Equal(MessageType.StickerMessage, message.Type);
+            Assert.Equal(stickerFile.FileId, message.Sticker.FileId);
         }
 
         // ToDo: Send one of the stickers retrieved to chat
-
         #endregion
-        
+
         private static class FactTitles
         {
-            public const string ShouldGetStickerSet = "Should get sticker set";            
+            public const string ShouldGetStickerSet = "Should get sticker set";
+            public const string ShouldSendSticker = "Should send sticker";
         }
     }
 }
