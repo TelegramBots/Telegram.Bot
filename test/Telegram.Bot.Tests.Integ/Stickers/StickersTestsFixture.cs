0a18f28085b81c33a65f2e8c2046b2026dacae44
u_m09ad <smbondar@alfabank.ru>
2017-11-28 12:48:25 +0300

    [+] StickersTestFixture class

---
+++ test/Telegram.Bot.Tests.Integ/Stickers/StickersTestsFixture.cs
@@ -0,0 +1,19 @@
+﻿using Telegram.Bot.Tests.Integ.Common;
+using Telegram.Bot.Types;
+
+namespace Telegram.Bot.Tests.Integ.Stickers
+{
+    public class StickersTestsFixture
+    {
+        public TestsFixture TestsFixture { get; }
+
+        public ChatId ChatId { get; set; }
+
+        public StickerSet StickerSet { get; set; }
+
+        public StickersTestsFixture(TestsFixture testsFixture)
+        {
+            TestsFixture = testsFixture;
+        }
+    }
+}
