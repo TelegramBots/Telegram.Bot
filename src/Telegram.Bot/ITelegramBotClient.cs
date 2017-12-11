118a535d38722bfa5c6e19903b53b7ddaac2fa56
Aleksey Usatov <usatov.aleksey@gmail.com>
2017-12-12 01:31:02 +0300

    Add sendAudio request

--- src/Telegram.Bot/ITelegramBotClient.cs
+++ src/Telegram.Bot/ITelegramBotClient.cs
@@ -311,11 +311,16 @@ namespace Telegram.Bot
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <returns>On success, the sent Description is returned.</returns>
         /// <see href="https://core.telegram.org/bots/api#sendaudio"/>
-        Task<Message> SendAudioAsync(ChatId chatId, FileToSend audio, string caption, int duration, string performer,
-            string title,
-            bool disableNotification = false,
-            int replyToMessageId = 0,
-            IReplyMarkup replyMarkup = null,
+        Task<Message> SendAudioAsync(
+            ChatId chatId,
+            FileToSend audio,
+            string caption = default,
+            int duration = default,
+            string performer = default,
+            string title = default,
+            bool disableNotification = default,
+            int replyToMessageId = default,
+            IReplyMarkup replyMarkup = default,
             CancellationToken cancellationToken = default);
 
         /// <summary>
---
+++ src/Telegram.Bot/Requests/SendAudioRequest.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Telegram.Bot.Helpers;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Telegram.Bot.Requests
+{
+    /// <summary>
+    /// Send audio files, if you want Telegram clients to display them in the music player. Your audio must be in the .mp3 format.
+    /// </summary>
+    public class SendAudioRequest : FileRequestBase<Message>
+    {
+        /// <summary>
+        /// Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+        /// </summary>
+        public ChatId ChatId { get; set; }
+
+        /// <summary>
+        /// Audio file to send
+        /// </summary>
+        public FileToSend Audio { get; set; }
+
+        /// <summary>
+        /// Photo caption (may also be used when resending photos by file_id), 0-200 characters
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Caption { get; set; }
+
+        /// <summary>
+        /// Duration of the audio in seconds
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Duration { get; set; }
+
+        /// <summary>
+        /// Performer
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Performer { get; set; }
+
+        /// <summary>
+        /// Track name
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Sends the message silently. Users will receive a notification with no sound.
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool DisableNotification { get; set; }
+
+        /// <summary>
+        /// If the message is a reply, ID of the original message
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int ReplyToMessageId { get; set; }
+
+        /// <summary>
+        /// Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public IReplyMarkup ReplyMarkup { get; set; }
+
+        /// <summary>
+        /// Initializes a new request
+        /// </summary>
+        public SendAudioRequest()
+            : base("sendAudio")
+        { }
+
+        /// <summary>
+        /// Initializes a new request with chatId and audio
+        /// </summary>
+        /// <param name="chatId">Unique identifier for the target chat or username of the target channel</param>
+        /// <param name="audio">Audio to send</param>
+        public SendAudioRequest(ChatId chatId, FileToSend audio)
+            : this()
+        {
+            ChatId = chatId;
+            Audio = audio;
+        }
+
+        /// <summary>
+        /// Generate content of HTTP message
+        /// </summary>
+        /// <param name="serializerSettings">JSON serialization setting</param>
+        /// <returns>Content of HTTP request</returns>
+        public override HttpContent ToHttpContent(JsonSerializerSettings serializerSettings)
+        {
+            HttpContent content;
+
+            if (Audio.Type == FileType.Stream)
+            {
+                var parameters = new Dictionary<string, object>
+                {
+                    { nameof(ChatId).ToSnakeCased(), ChatId},
+                    { nameof(Audio).ToSnakeCased(), Audio },
+                    { nameof(Caption).ToSnakeCased(), Caption },
+                    { nameof(Performer).ToSnakeCased(), Performer },
+                    { nameof(Title).ToSnakeCased(), Title },
+                    { nameof(ReplyMarkup).ToSnakeCased(), ReplyMarkup }
+                };
+
+                if (Duration != default)
+                {
+                    parameters.Add(nameof(Duration).ToSnakeCased(), Duration);
+                }
+
+                if (ReplyToMessageId != default)
+                {
+                    parameters.Add(nameof(ReplyToMessageId).ToSnakeCased(), ReplyToMessageId);
+                }
+
+                if (DisableNotification != default)
+                {
+                    parameters.Add(nameof(DisableNotification).ToSnakeCased(), DisableNotification);
+                }
+
+                content = GetMultipartContent(parameters, serializerSettings);
+            }
+            else
+            {
+                content = base.ToHttpContent(serializerSettings);
+            }
+
+            return content;
+        }
+    }
+}
