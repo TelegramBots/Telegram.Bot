46e3373ee61e987b1240664f5d4c08d6b7e533cf
Poulad <poulad.pld@gmail.com>
2018-05-05 20:20:51 -0400

    test

--- test/Telegram.Bot.Tests.Integ/Framework/XunitExtensions/XunitCustomTestMethodRunner.cs
+++ test/Telegram.Bot.Tests.Integ/Framework/XunitExtensions/XunitCustomTestMethodRunner.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Polly;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
@@ -11,36 +10,56 @@ namespace Telegram.Bot.Tests.Integ.Framework.XunitExtensions
     public class XunitCustomTestMethodRunner : XunitTestMethodRunner
     {
         private readonly IMessageSink _diagnosticMessageSink;
+
         private readonly object[] _constructorArguments;
 
-        public XunitCustomTestMethodRunner(ITestMethod testMethod,
-                                           IReflectionTypeInfo @class,
-                                           IReflectionMethodInfo method,
-                                           IEnumerable<IXunitTestCase> testCases,
-                                           IMessageSink diagnosticMessageSink,
-                                           IMessageBus messageBus,
-                                           ExceptionAggregator aggregator,
-                                           CancellationTokenSource cancellationTokenSource,
-                                           object[] constructorArguments)
-            : base(testMethod, @class, method, testCases, diagnosticMessageSink, messageBus, aggregator, cancellationTokenSource, constructorArguments)
+        public XunitCustomTestMethodRunner(
+            ITestMethod testMethod,
+            IReflectionTypeInfo @class,
+            IReflectionMethodInfo method,
+            IEnumerable<IXunitTestCase> testCases,
+            IMessageSink diagnosticMessageSink,
+            IMessageBus messageBus,
+            ExceptionAggregator aggregator,
+            CancellationTokenSource cancellationTokenSource,
+            object[] constructorArguments
+        )
+            : base(testMethod, @class, method, testCases, diagnosticMessageSink, messageBus, aggregator,
+                cancellationTokenSource, constructorArguments)
         {
             _diagnosticMessageSink = diagnosticMessageSink;
             _constructorArguments = constructorArguments;
         }
 
-        protected override Task<RunSummary> RunTestCaseAsync(IXunitTestCase testCase) =>
-            Policy
-                .HandleResult<RunSummary>(r => r.Failed != 0)
-                .WaitAndRetryAsync(new[]
-                {
-                    TimeSpan.FromSeconds(30),
-                    TimeSpan.FromSeconds(45),
-                    TimeSpan.FromSeconds(60),
-                })
-                .ExecuteAsync(() =>
-                    // We've done everything we need, so let the built-in types do the rest of the heavy lifting
-                    testCase.RunAsync
-                        (_diagnosticMessageSink, MessageBus, _constructorArguments, new ExceptionAggregator(Aggregator), CancellationTokenSource)
+        protected override async Task<RunSummary> RunTestCaseAsync(IXunitTestCase testCase)
+        {
+            const int maxRetry = 1;
+            const int delaySeconds = 30;
+
+            RunSummary summary = null;
+            int retryCount = 0;
+            while (retryCount < maxRetry)
+            {
+                summary = await testCase.RunAsync(
+                    _diagnosticMessageSink,
+                    MessageBus,
+                    _constructorArguments,
+                    new ExceptionAggregator(Aggregator),
+                    CancellationTokenSource
                 );
+
+                if (summary.Failed == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    await Task.Delay(delaySeconds * 1000);
+                    retryCount++;
+                }
+            }
+
+            return summary;
+        }
     }
 }
