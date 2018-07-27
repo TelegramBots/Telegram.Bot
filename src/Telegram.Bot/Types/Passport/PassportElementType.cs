d8885eb9eab3fbe3e117e8c420b3586ea5f68fdd
Aleksey Usatov <usatov.aleksey@gmail.com>
2018-07-27 17:18:09 +0300

    Add passport types

---
+++ src/Telegram.Bot/Types/Passport/PassportElementType.cs
@@ -0,0 +1,73 @@
+﻿namespace Telegram.Bot.Types.Passport
+{
+    /// <summary>
+    /// Passport element type.
+    /// </summary>
+    public static class PassportElementType
+    {
+        /// <summary>
+        /// Personal details
+        /// </summary>
+        public const string PersonalDetails = "personal_details";
+
+        /// <summary>
+        /// Passport
+        /// </summary>
+        public const string Passport = "passport";
+
+        /// <summary>
+        /// Driver license
+        /// </summary>
+        public const string DriverLicense = "driver_license";
+
+        /// <summary>
+        /// Identity card
+        /// </summary>
+        public const string IdentityCard = "identity_card";
+
+        /// <summary>
+        /// Internal passport
+        /// </summary>
+        public const string InternalPassport = "internal_passport";
+
+        /// <summary>
+        /// Address
+        /// </summary>
+        public const string Address = "address";
+
+        /// <summary>
+        /// Utility bill
+        /// </summary>
+        public const string UtilityBill = "utility_bill";
+
+        /// <summary>
+        /// Bank statement
+        /// </summary>
+        public const string BankStatement = "bank_statement";
+
+        /// <summary>
+        /// Rental agreement
+        /// </summary>
+        public const string RentalAgreement = "rental_agreement";
+
+        /// <summary>
+        /// Passport registration
+        /// </summary>
+        public const string PassportRegistration = "passport_registration";
+
+        /// <summary>
+        /// Temporary registration
+        /// </summary>
+        public const string TemporaryRegistration = "temporary_registration";
+
+        /// <summary>
+        /// Phone number
+        /// </summary>
+        public const string PhoneNumber = "phone_number";
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        public const string Email = "email";
+    }
+}
