ec0cc70169a1ebae591238bb63f994f3b3e632a7
Miha Zupan <mihazupan.zupan1@gmail.com>
2018-07-28 19:49:39 +0200

    Added missing passport types

---
+++ src/Telegram.Bot/Types/Passport/IdDocumentData.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Telegram.Bot.Types.Passport
+{
+    /// <summary>
+    /// This object represents the data of an identity document.
+    /// </summary>
+    [JsonObject(MemberSerialization.OptIn, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class IdDocumentData
+    {
+        /// <summary>
+        /// Document number
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public string DocumentNo;
+
+        /// <summary>
+        /// Optional. Date of expiry, in DD.MM.YYYY format
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string ExpiryDate;
+    }
+}
