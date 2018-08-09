abb77c8acc11a04f46277dcbc1be8578d41635c9
Miha Zupan <mihazupan.zupan1@gmail.com>
2018-08-09 21:37:49 +0200

    Add Passport File Decryption tests

--- test/Telegram.Bot.Tests.Unit/Passport/PassportTests.cs
+++ test/Telegram.Bot.Tests.Unit/Passport/PassportTests.cs
@@ -1,7 +1,9 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Passport;
 using Xunit;
@@ -56,21 +58,26 @@ namespace Telegram.Bot.Tests.Unit.Passport
                             break;
 
                         case PassportElementType.Passport:
-                            Assert.True(element.DecryptData<IdDocumentData>(credentials.Passport.Data).Successful);
-                            Assert.False(element.DecryptData<PersonalDetails>(credentials.Passport.Data).Successful);
-                            Assert.False(element.DecryptData<ResidentialAddress>(credentials.Passport.Data).Successful);
+                            Assert.True(element.DecryptData<IdDocumentData>(credentials.Passport.Data)
+                                .Successful);
+                            Assert.False(element.DecryptData<PersonalDetails>(credentials.Passport.Data)
+                                .Successful);
+                            Assert.False(element.DecryptData<ResidentialAddress>(credentials.Passport.Data)
+                                .Successful);
                             break;
 
                         case PassportElementType.DriverLicense:
-                            Assert.True(element.DecryptData<IdDocumentData>(credentials.DriverLicense.Data).Successful);
-                            Assert.False(
-                                element.DecryptData<PersonalDetails>(credentials.DriverLicense.Data).Successful);
+                            Assert.True(element.DecryptData<IdDocumentData>(credentials.DriverLicense.Data)
+                                .Successful);
+                            Assert.False(element.DecryptData<PersonalDetails>(credentials.DriverLicense.Data)
+                                .Successful);
                             Assert.False(element.DecryptData<ResidentialAddress>(credentials.DriverLicense.Data)
                                 .Successful);
                             break;
 
                         case PassportElementType.IdentityCard:
-                            Assert.True(element.DecryptData<IdDocumentData>(credentials.IdentityCard.Data).Successful);
+                            Assert.True(element.DecryptData<IdDocumentData>(credentials.IdentityCard.Data)
+                                .Successful);
                             Assert.False(element.DecryptData<PersonalDetails>(credentials.IdentityCard.Data)
                                 .Successful);
                             Assert.False(element.DecryptData<ResidentialAddress>(credentials.IdentityCard.Data)
@@ -87,9 +94,12 @@ namespace Telegram.Bot.Tests.Unit.Passport
                             break;
 
                         case PassportElementType.Address:
-                            Assert.True(element.DecryptData<ResidentialAddress>(credentials.Address.Data).Successful);
-                            Assert.False(element.DecryptData<PersonalDetails>(credentials.Address.Data).Successful);
-                            Assert.False(element.DecryptData<IdDocumentData>(credentials.Address.Data).Successful);
+                            Assert.True(element.DecryptData<ResidentialAddress>(credentials.Address.Data)
+                                .Successful);
+                            Assert.False(element.DecryptData<PersonalDetails>(credentials.Address.Data)
+                                .Successful);
+                            Assert.False(element.DecryptData<IdDocumentData>(credentials.Address.Data)
+                                .Successful);
                             break;
 
                         default:
@@ -98,24 +108,122 @@ namespace Telegram.Bot.Tests.Unit.Passport
                 }
                 else
                 {
-                    Assert.False(element.DecryptData<PersonalDetails>(null).Successful);
-                    Assert.False(element.DecryptData<IdDocumentData>(null).Successful);
-                    Assert.False(element.DecryptData<ResidentialAddress>(null).Successful);
+                    Assert.False(element.DecryptData<PersonalDetails>(null)
+                        .Successful);
+                    Assert.False(element.DecryptData<IdDocumentData>(null)
+                        .Successful);
+                    Assert.False(element.DecryptData<ResidentialAddress>(null)
+                        .Successful);
                 }
             }
         }
 
+        // ToDo - needs access to the bot token - secure value to be added to config - see Should_DecryptFile
+        #pragma warning disable CS0162 // Unreachable code
         [Fact]
-        public void Should_DecryptFileFields()
+        public async Task Should_DecryptFileFields()
         {
+            return;
+
+            PassportData passportData = ReadPassportMessage().PassportData;
+            RSA privateKey = ReadPrivateKey();
+
+            Assert.True(PassportCryptography.TryDecryptCredentials(passportData.Credentials, privateKey, out Credentials masterCredentials));
+
+            SecureData credentials = masterCredentials.SecureData;
+            foreach (EncryptedPassportElement element in passportData.Data)
+            {
+                switch (element.Type)
+                {
+                    case PassportElementType.UtilityBill:
+                        await Should_DecryptFiles(element.Files, credentials.UtilityBill.Files);
+                        break;
+
+                    case PassportElementType.BankStatement:
+                        await Should_DecryptFiles(element.Files, credentials.BankStatement.Files);
+                        break;
+
+                    case PassportElementType.RentalAgreement:
+                        await Should_DecryptFiles(element.Files, credentials.RentalAgreement.Files);
+                        break;
+
+                    case PassportElementType.Passport:
+                        await Should_DecryptFiles(element.Files, credentials.Passport.Files);
+                        await Should_DecryptFile(element.FrontSide, credentials.Passport.FrontSide);
+                        if (element.Selfie != null)
+                        {
+                            await Should_DecryptFile(element.Selfie, credentials.Passport.Selfie);
+                        }
+                        break;
+
+                    case PassportElementType.TemporaryRegistration:
+                        await Should_DecryptFiles(element.Files, credentials.TemporaryRegistration.Files);
+                        break;
+
+                    case PassportElementType.DriverLicense:
+                        await Should_DecryptFile(element.FrontSide, credentials.DriverLicense.FrontSide);
+                        await Should_DecryptFile(element.ReverseSide, credentials.DriverLicense.ReverseSide);
+                        if (element.Selfie != null)
+                        {
+                            await Should_DecryptFile(element.Selfie, credentials.DriverLicense.Selfie);
+                        }
+                        break;
+
+                    case PassportElementType.IdentityCard:
+                        await Should_DecryptFile(element.FrontSide, credentials.IdentityCard.FrontSide);
+                        await Should_DecryptFile(element.ReverseSide, credentials.IdentityCard.ReverseSide);
+                        if (element.Selfie != null)
+                        {
+                            await Should_DecryptFile(element.Selfie, credentials.IdentityCard.Selfie);
+                        }
+                        break;
+
+                    case PassportElementType.InternalPassport:
+                        await Should_DecryptFile(element.FrontSide, credentials.InternalPassport.FrontSide);
+                        if (element.Selfie != null)
+                        {
+                            await Should_DecryptFile(element.Selfie, credentials.InternalPassport.Selfie);
+                        }
+                        break;
+                }
+            }
         }
+
+        private async Task Should_DecryptFiles(PassportFile[] passportFiles, FileCredentials[] credentials)
+        {
+            Assert.NotNull(passportFiles);
+            Assert.NotNull(credentials);
+            Assert.NotEmpty(passportFiles);
+            Assert.NotEmpty(credentials);
+            Assert.Equal(passportFiles.Length, credentials.Length);
+            for (int i = 0; i < passportFiles.Length; i++)
+            {
+                await Should_DecryptFile(passportFiles[i], credentials[i]);
+            }
+        }
+        private async Task Should_DecryptFile(PassportFile passportFile, FileCredentials credentials)
+        {
+            Assert.NotNull(passportFile);
+            Assert.NotNull(credentials);
+            string token = "ToDo - get token from configuration";
+            TelegramBotClient bot = new TelegramBotClient(token);
+            byte[] encryptedFile;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                await bot.GetInfoAndDownloadFileAsync(passportFile.FileId, ms);
+                encryptedFile = ms.ToArray();
+            }
+            Assert.NotEmpty(encryptedFile);
+            Assert.True(PassportCryptography.TryDecryptFile(encryptedFile, credentials, out byte[] decrypted));
+            Assert.NotEmpty(decrypted);
+        }
+        #pragma warning restore CS0162 // Unreachable code
 
         private Message ReadPassportMessage()
         {
             string passportMessageJson = System.IO.File.ReadAllText(Constants.FileNames.Passport.PassportMessage);
             return JsonConvert.DeserializeObject<Message>(passportMessageJson);
         }
-
         private RSA ReadPrivateKey()
         {
             string privateKeyJson = System.IO.File.ReadAllText(Constants.FileNames.Passport.PrivateKey);
