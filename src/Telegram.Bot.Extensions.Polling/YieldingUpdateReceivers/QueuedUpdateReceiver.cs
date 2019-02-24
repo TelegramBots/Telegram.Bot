bdb509addb579a0085b76d11c72995945382c489
Miha Zupan <mihazupan.zupan1@gmail.com>
2019-02-24 01:31:07 +0100

    Implement QueuedUpdateReceiver

--- src/Telegram.Bot.Extensions.Polling/YieldingUpdateReceivers/QueuedUpdateReceiver.cs
+++ src/Telegram.Bot.Extensions.Polling/YieldingUpdateReceivers/QueuedUpdateReceiver.cs
@@ -10,6 +10,8 @@ namespace Telegram.Bot.Extensions.Polling
 {
     public class QueuedUpdateReceiver : IYieldingUpdateReceiver
     {
+        private static readonly Update[] EmptyUpdates = { };
+
         public readonly ITelegramBotClient BotClient;
 
         public QueuedUpdateReceiver(ITelegramBotClient botClient)
@@ -17,13 +19,89 @@ namespace Telegram.Bot.Extensions.Polling
             BotClient = botClient;
         }
 
+        public bool StartedReceiving { get; private set; }
+
         public async IAsyncEnumerable<Update> YieldUpdatesAsync(
             UpdateType[]? allowedUpdates = default,
-            Func<Exception, Task>? exceptionHandler = default,
+            Func<Exception, Task>? errorHandler = default,
             CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
-            yield return new Update();
+            if (StartedReceiving) throw new InvalidOperationException("YieldUpdatesAsync has already been called");
+            StartedReceiving = true;
+
+            // Only access producerQueue and exchange tcs under a lock
+            // We could use other objects instead, but this way it's very obvious what we're doing
+            object lockObject = new object();
+
+            List<Update[]> producerQueue = new List<Update[]>();
+            List<Update[]> consumerQueue = new List<Update[]>();
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+            _ = Task.Run(async () =>
+            {
+                int messageOffset = 0;
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    int timeout = (int)BotClient.Timeout.TotalSeconds;
+                    var updates = EmptyUpdates;
+                    try
+                    {
+                        updates = await BotClient.GetUpdatesAsync(
+                            messageOffset,
+                            timeout: timeout,
+                            allowedUpdates: allowedUpdates,
+                            cancellationToken: cancellationToken
+                        ).ConfigureAwait(false);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // Ignore
+                    }
+                    catch (Exception ex)
+                    {
+                        if (errorHandler != null)
+                        {
+                            // If the error handler throws then the consumer of this IAsyncEnumerable will wait forever
+                            await errorHandler(ex).ConfigureAwait(false);
+                        }
+                    }
+
+                    if (updates.Length > 0)
+                    {
+                        lock (lockObject)
+                        {
+                            producerQueue.Add(updates);
+                            tcs.TrySetResult(true);
+                        }
+                        messageOffset = updates[^1].Id + 1;
+                    }
+                }
+            });
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                // Don't await if it's obvious we already have updates
+                if (producerQueue.Count == 0)
+                    await tcs.Task.ConfigureAwait(false);
+
+                lock (lockObject)
+                {
+                    // Swap
+                    var temp = producerQueue;
+                    producerQueue = consumerQueue;
+                    consumerQueue = temp;
+                }
+
+                foreach (var updateArray in consumerQueue)
+                    foreach (var update in updateArray)
+                        yield return update;
+
+                consumerQueue.Clear();
+
+                // We could save an allocation here by having a custom TCS,
+                // but it does not matter as we're also allocating Updates and making network requests
+                lock (lockObject) tcs = new TaskCompletionSource<bool>();
+            }
         }
     }
 }
