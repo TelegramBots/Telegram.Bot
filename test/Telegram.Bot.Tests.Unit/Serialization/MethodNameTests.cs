3d5c5995e7b6645b55539eeb26de53ef6b80216f
Fabio Di Peri <fabiodiperi@outlook.com>
2019-11-21 23:21:47 +0100

    Fix tests

--- test/Telegram.Bot.Tests.Unit/Serialization/MethodNameTests.cs
+++ test/Telegram.Bot.Tests.Unit/Serialization/MethodNameTests.cs
@@ -14,19 +14,19 @@ namespace Telegram.Bot.Tests.Unit.Serialization
                 IsWebhookResponse = true
             };
 
-            var request = JsonConvert.SerializeObject(sendMessageRequest);
+            string request = JsonConvert.SerializeObject(sendMessageRequest);
             Assert.Contains(@"""method"":""sendMessage""", request);
         }
 
-        [Fact(DisplayName = "Should not serialize method name in webhook responses")]
-        public void Should_Not_Serialize_MethodName_In_Webhook_Responses()
+        [Fact(DisplayName = "Should not serialize method name when not a webhook responses")]
+        public void Should_Not_Serialize_MethodName_When_Not_In_Webhook_Responses()
         {
             SendMessageRequest sendMessageRequest = new SendMessageRequest(1, "text")
             {
                 IsWebhookResponse = false
             };
 
-            var request = JsonConvert.SerializeObject(sendMessageRequest);
+            string request = JsonConvert.SerializeObject(sendMessageRequest);
             Assert.DoesNotContain(@"""method"":""sendMessage""", request);
         }
     }
