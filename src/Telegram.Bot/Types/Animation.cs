dc5c430c41374884dedf4db52b0542dc7452d4bb
Aleksey Usatov <usatov.aleksey@gmail.com>
2020-01-03 16:51:05 +0300

    Add missing property to Animation

--- src/Telegram.Bot/Types/Animation.cs
+++ src/Telegram.Bot/Types/Animation.cs
@@ -16,6 +16,12 @@ namespace Telegram.Bot.Types
         public string FileId { get; set; }
 
         /// <summary>
+        /// Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public string FileUniqueId { get; set; }
+
+        /// <summary>
         /// Video width as defined by sender
         /// </summary>
         [JsonProperty(Required = Required.Always)]
