c3ff030dd5df012934059a00db5deb22dd0c2733
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2020-01-09 22:31:35 +0200

    SetChatDescriptionRequest.cs

--- src/Telegram.Bot.Core/Requests/Available Methods/Admin Bot/SetChatDescriptionRequest.cs
+++ src/Telegram.Bot.Core/Requests/Available Methods/Admin Bot/SetChatDescriptionRequest.cs
@@ -1,26 +1,37 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
 using Telegram.Bot.Types;
 
 // ReSharper disable once CheckNamespace
 namespace Telegram.Bot.Requests
 {
     /// <summary>
-    /// Change the description of a supergroup or a channel. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights.
+    /// Change the description of a group, a supergroup or a channel.
+    /// The bot must be an administrator in the chat for this to work and must have the appropriate admin rights.
     /// </summary>
-    public sealed class SetChatDescriptionRequest : ChatIdRequestBase<bool>
+    [DataContract]
+    public sealed class SetChatDescriptionRequest : RequestBase<bool>
     {
         /// <summary>
-        /// New chat Description, 0-255 characters
+        /// Unique identifier for the target chat or username of the target channel (in the format @channel_username)
         /// </summary>
-        [AllowNull]
+        [DataMember(IsRequired = true), NotNull]
+        public ChatId ChatId { get; }
+
+        /// <summary>
+        /// New chat description, 0-255 characters
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
         public string? Description { get; set; }
 
         /// <summary>
-        /// Initializes a new request with <see cref="ChatId"/> and new <see cref="Description"/>
+        /// Initializes a new request of type <see cref="SetChatDescriptionRequest"/>
         /// </summary>
-        /// <param name="chatId">Unique identifier for the target chat or username of the target channel</param>
-        /// <param name="description">New chat Description, 0-255 characters</param>
-        public SetChatDescriptionRequest([NotNull] ChatId chatId, [AllowNull] string? description = default)
+        /// <param name="chatId">Unique identifier for the target chat or username of the target channel (in the format @channel_username)</param>
+        /// <param name="description">New chat description, 0-255 characters</param>
+        public SetChatDescriptionRequest(
+            [DisallowNull] ChatId chatId,
+            [AllowNull] string? description = default)
             : base("setChatDescription")
         {
             ChatId = chatId;
