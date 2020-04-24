deb5e735d7b6d77d69518ac429b0b61653e35db2
Aleksey Usatov <usatov.aleksey@gmail.com>
2020-04-24 22:51:05 +0300

    Add new poll tests

---
+++ test/Telegram.Bot.Tests.Integ/Polls/SelfStoppingPollTestsFixture.cs
@@ -0,0 +1,19 @@
+using System;
+using Telegram.Bot.Tests.Integ.Framework;
+using Telegram.Bot.Types;
+
+namespace Telegram.Bot.Tests.Integ.Polls
+{
+    public class SelfStoppingPollTestsFixture
+    {
+        public TestsFixture TestsFixture { get; }
+        public Message PollMessage { get; set; }
+        public int OpenPeriod { get; set; }
+        public DateTime CloseDate { get; set; }
+
+        public SelfStoppingPollTestsFixture(TestsFixture testsFixture)
+        {
+            TestsFixture = testsFixture;
+        }
+    }
+}
