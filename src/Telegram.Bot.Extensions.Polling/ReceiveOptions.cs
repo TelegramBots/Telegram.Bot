2ec2d111366f2349d89fffe64137d5211439305e
Aleksey Usatov <usatov.aleksey@gmail.com>
2020-06-22 01:18:04 +0300

    Add validation to Limit property in ReceiveOptions

--- src/Telegram.Bot.Extensions.Polling/ReceiveOptions.cs
+++ src/Telegram.Bot.Extensions.Polling/ReceiveOptions.cs
@@ -1,3 +1,4 @@
+using System;
 using Telegram.Bot.Types.Enums;
 
 namespace Telegram.Bot.Extensions.Polling
@@ -7,19 +8,39 @@ namespace Telegram.Bot.Extensions.Polling
     /// </summary>
     public sealed class ReceiveOptions
     {
+        private int? _limit;
+
         /// <summary>
-        /// Identifier of the first update to be returned
+        /// Identifier of the first update to be returned.
         /// </summary>
         public int? Offset { get; set; }
 
         /// <summary>
-        /// Indicates which <see cref="UpdateType"/>s are allowed to be received. <c>null</c> means to leave it as it
-        /// was set in previous getUpdates call
+        /// Indicates which <see cref="UpdateType"/>s are allowed to be received.
+        /// In case of <c>null</c> the previous setting will be used
         /// </summary>
         public UpdateType[]? AllowedUpdates { get; set; }
         /// <summary>
-        /// Limits the number of updates to be retrieved. Values between 1-100 are accepted. Defaults to 100.
+        /// Limits the number of updates to be retrieved. Values between 1-100 are accepted.
+        /// Defaults to 100 when is set to <c>null</c>.
         /// </summary>
-        public int? Limit { get; set; }
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when the value doesn't satisfies constraints
+        /// </exception>
+        public int? Limit
+        {
+            get => _limit;
+            set
+            {
+                if (value < 1 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(value),
+                        value,
+                        $"'{nameof(Limit)}' can not be less than 1 or greater than 100");
+                }
+                _limit = value;
+            }
+        }
     }
 }
