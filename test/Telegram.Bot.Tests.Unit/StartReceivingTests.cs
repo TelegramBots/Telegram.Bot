a58ac5d3f489789ff50cc7a00768680c8126c791
nminaya <nelsonminayaperez@gmail.com>
2020-10-04 09:40:05 -0400

    Update StartReceivingTests.cs

--- test/Telegram.Bot.Tests.Unit/StartReceivingTests.cs
+++ test/Telegram.Bot.Tests.Unit/StartReceivingTests.cs
@@ -1,4 +1,7 @@
+using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Tests.Unit.Framework;
 using Xunit;
 
 namespace Telegram.Bot.Tests.Unit
@@ -9,7 +12,8 @@ namespace Telegram.Bot.Tests.Unit
         public void Should_Not_ReceiveUpdates_On_CancelledToken()
         {
             // Arrange
-            var botClient = new TelegramBotClient("1234567:4TT8bAc8GHUspu3ERYn-KGcvsvGB9u_n4ddy");
+            var httpClientCounter = new RequestCounterHttpClientHandler();
+            var botClient = new TelegramBotClient("1234567:4TT8bAc8GHUspu3ERYn-KGcvsvGB9u_n4ddy", new HttpClient(httpClientCounter));
             var tokenSource = new CancellationTokenSource();
 
             tokenSource.Cancel();
@@ -19,6 +23,24 @@ namespace Telegram.Bot.Tests.Unit
 
             // Assert
             Assert.False(botClient.IsReceiving);
+            Assert.Equal(0, httpClientCounter.RequestCounter);
+        }
+
+        [Fact]
+        public async Task Should_ReceiveUpdates_On_ActiveToken()
+        {
+            // Arrange
+            var httpClientCounter = new RequestCounterHttpClientHandler();
+            var botClient = new TelegramBotClient("1234567:4TT8bAc8GHUspu3ERYn-KGcvsvGB9u_n4ddy", new HttpClient(httpClientCounter));
+            var tokenSource = new CancellationTokenSource();
+
+            // Act
+            var cancelTokenTask = Task.Delay(500).ContinueWith((t) => tokenSource.Cancel()); // Cancel Token after 500ms
+            botClient.StartReceiving(cancellationToken: tokenSource.Token);
+            await cancelTokenTask;
+
+            // Assert
+            Assert.NotEqual(0, httpClientCounter.RequestCounter);
         }
     }
 }
