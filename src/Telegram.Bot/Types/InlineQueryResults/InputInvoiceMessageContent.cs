e4b13ac0742a7c5301d298631e58908c50a10771
Sinys <alex.fedorus@gmail.com>
2021-05-02 00:30:48 +0300

    Added the type InputInvoiceMessageContent to support sending invoices as inline query results.

--- src/Telegram.Bot/Types/InlineQueryResults/InputInvoiceMessageContent.cs
+++ src/Telegram.Bot/Types/InlineQueryResults/InputInvoiceMessageContent.cs
@@ -1,70 +1,64 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using Telegram.Bot.Requests.Abstractions;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.Payments;
-using Telegram.Bot.Types.ReplyMarkups;
 
-// ReSharper disable once CheckNamespace
-namespace Telegram.Bot.Requests
+namespace Telegram.Bot.Types.InlineQueryResults
 {
     /// <summary>
-    /// Send invoices. On success, the sent <see cref="Message"/> is returned.
+    /// Represents the content of an invoice message to be sent as the result of an inline query.
     /// </summary>
     [JsonObject(MemberSerialization.OptIn, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
-    public class SendInvoiceRequest : RequestBase<Message>,
-                                      INotifiableMessage,
-                                      IReplyMessage,
-                                      IInlineReplyMarkupMessage
+    public class InputInvoiceMessageContent : InputMessageContentBase
     {
-        /// <summary>
-        /// Unique identifier for the target private chat
-        /// </summary>
-        [JsonProperty(Required = Required.Always)]
-        public long ChatId { get; }
-
         /// <summary>
         /// Product name, 1-32 characters
         /// </summary>
         [JsonProperty(Required = Required.Always)]
-        public string Title { get; }
+        public string Title { get; set; }
 
         /// <summary>
         /// Product description, 1-255 characters
         /// </summary>
         [JsonProperty(Required = Required.Always)]
-        public string Description { get; }
+        public string Description { get; set; }
 
         /// <summary>
         /// Bot-defined invoice payload, 1-128 bytes. This will not be displayed to the user, use for your internal processes.
         /// </summary>
         [JsonProperty(Required = Required.Always)]
-        public string Payload { get; }
+        public string Payload { get; set; }
 
         /// <summary>
         /// Payments provider token, obtained via BotFather
         /// </summary>
         [JsonProperty(Required = Required.Always)]
-        public string ProviderToken { get; }
-
-        /// <summary>
-        /// Optional. Unique deep-linking parameter. If left empty, forwarded copies of the sent message will have a Pay button, allowing multiple users to pay directly from the forwarded message, using the same invoice. If non-empty, forwarded copies of the sent message will have a URL button with a deep link to the bot (instead of a Pay button), with the value used as the start parameter
-        /// </summary>
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string StartParameter { get; set; }
+        public string ProviderToken { get; set; }
 
         /// <summary>
         /// Three-letter ISO 4217 currency code
         /// </summary>
         [JsonProperty(Required = Required.Always)]
-        public string Currency { get; }
+        public string Currency { get; set; }
 
         /// <summary>
         /// Price breakdown, a list of components (e.g. product price, tax, discount, delivery cost, delivery tax, bonus, etc.)
         /// </summary>
         [JsonProperty(Required = Required.Always)]
         public IEnumerable<LabeledPrice> Prices { get; }
+
+        /// <summary>
+        /// Optional. The maximum accepted amount for tips in the smallest units of the currency (integer, not float/double). For example, for a maximum tip of US$ 1.45 pass max_tip_amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies). Defaults to 0
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int MaxTipAmount { get; set; }
+
+
+        /// <summary>
+        /// Optional. A JSON-serialized array of suggested amounts of tips in the smallest units of the currency (integer, not float/double). At most 4 suggested tip amounts can be specified. The suggested tip amounts must be positive, passed in a strictly increased order and must not exceed max_tip_amount.
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int[] SuggestedTipAmounts { get; set; }
 
         /// <summary>
         /// JSON-encoded data about the invoice, which will be shared with the payment provider. A detailed description of required fields should be provided by the payment provider.
@@ -138,59 +132,30 @@ namespace Telegram.Bot.Requests
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public bool IsFlexible { get; set; }
 
-        /// <inheritdoc />
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool DisableNotification { get; set; }
-
-        /// <inheritdoc />
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int ReplyToMessageId { get; set; }
-
-        /// <inheritdoc />
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool AllowSendingWithoutReply { get; set; }
-
-        /// <summary>
-        /// A JSON-serialized object for an inline keyboard. If empty, one 'Pay total price' button will be shown. If not empty, the first button must be a Pay button.
-        /// </summary>
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public InlineKeyboardMarkup ReplyMarkup { get; set; }
-
-        /// <summary>
-        /// Optional. The maximum accepted amount for tips in the smallest units of the currency (integer, not float/double). For example, for a maximum tip of US$ 1.45 pass max_tip_amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies). Defaults to 0
-        /// </summary>
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int MaxTipAmount { get; set; }
+        private InputInvoiceMessageContent()
+        {
+        }
 
 
         /// <summary>
-        /// Optional. A JSON-serialized array of suggested amounts of tips in the smallest units of the currency (integer, not float/double). At most 4 suggested tip amounts can be specified. The suggested tip amounts must be positive, passed in a strictly increased order and must not exceed max_tip_amount.
+        /// Initializes with title, description, payload, providerToken, currency and an array of <see cref="LabeledPrice"/>
         /// </summary>
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int[] SuggestedTipAmounts { get; set; }
-
-        /// <summary>
-        /// Initializes a new request with chatId, title, description, payload, providerToken, currency and an array of <see cref="LabeledPrice"/>
-        /// </summary>
-        /// <param name="chatId">Unique identifier for the target private chat</param>
         /// <param name="title">Product name, 1-32 characters</param>
         /// <param name="description">Product description, 1-255 characters</param>
         /// <param name="payload">Bot-defined invoice payload, 1-128 bytes</param>
-        /// <param name="providerToken">Payments provider token, obtained via Botfather</param>
+        /// <param name="providerToken">Payments provider token, obtained via BotFather</param>
         /// <param name="currency">Three-letter ISO 4217 currency code</param>
         /// <param name="prices">Price breakdown, a list of components (e.g. product price, tax, discount, delivery cost, delivery tax, bonus, etc.)</param>
-        public SendInvoiceRequest(
-            long chatId,
+
+        public InputInvoiceMessageContent(
             string title,
             string description,
             string payload,
             string providerToken,
             string currency,
             IEnumerable<LabeledPrice> prices
-        )
-            : base("sendInvoice")
+            )
         {
-            ChatId = chatId;
             Title = title;
             Description = description;
             Payload = payload;
