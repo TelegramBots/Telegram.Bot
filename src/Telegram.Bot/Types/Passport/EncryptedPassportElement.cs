566055e91a15b06beec5312df69323c8c3a07f29
Aleksey Usatov <usatov.aleksey@gmail.com>
2021-07-04 15:57:13 +0300

    Added EncryptedPassportElementType enum

--- src/Telegram.Bot/Types/Passport/EncryptedPassportElement.cs
+++ src/Telegram.Bot/Types/Passport/EncryptedPassportElement.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using static Telegram.Bot.Types.Passport.EncryptedPassportElementType;
 
 // ReSharper disable once CheckNamespace
 namespace Telegram.Bot.Types.Passport
@@ -11,36 +12,36 @@ namespace Telegram.Bot.Types.Passport
     public class EncryptedPassportElement
     {
         /// <summary>
-        /// Element type. One of "personal_details", "passport", "driver_license", "identity_card",
-        /// "internal_passport", "address", "utility_bill", "bank_statement", "rental_agreement",
-        /// "passport_registration", "temporary_registration", "phone_number", "email".
+        /// Element type. One of <see cref="EncryptedPassportElementType"/>
         /// </summary>
         [JsonProperty(Required = Required.Always)]
-        public string Type { get; set; }
+        public EncryptedPassportElementType Type { get; set; }
 
         /// <summary>
         /// Optional. Base64-encoded encrypted Telegram Passport element data provided by the user, available for
-        /// "personal_details", "passport", "driver_license", "identity_card", "identity_passport" and "address"
+        /// <see cref="PersonalDetails"/>, <see cref="Passport"/>, <see cref="DriverLicence"/>,
+        /// <see cref="IdentityCard"/>, <see cref="InternalPassport"/> and <see cref="Address"/>
         /// types. Can be decrypted and verified using the accompanying <see cref="EncryptedCredentials"/>.
         /// </summary>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? Data { get; set; }
 
         /// <summary>
-        /// Optional. User's verified phone number, available only for "phone_number" type.
+        /// Optional. User's verified phone number, available only for <see cref="PhoneNumber"/> type.
         /// </summary>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? PhoneNumber { get; set; }
 
         /// <summary>
-        /// Optional. User's verified email address, available only for "email" type.
+        /// Optional. User's verified email address, available only for <see cref="Email"/> type.
         /// </summary>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? Email { get; set; }
 
         /// <summary>
-        /// Optional. Array of encrypted files with documents provided by the user, available for "utility_bill",
-        /// "bank_statement", "rental_agreement", "passport_registration" and "temporary_registration" types.
+        /// Optional. Array of encrypted files with documents provided by the user, available for
+        /// <see cref="UtilityBill"/>, <see cref="BankStatement"/>, <see cref="RentalAgreement"/>,
+        /// <see cref="PassportRegistration"/> and <see cref="TemporaryRegistration"/> types.
         /// Files can be decrypted and verified using the accompanying <see cref="EncryptedCredentials"/>.
         /// </summary>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
@@ -48,33 +49,36 @@ namespace Telegram.Bot.Types.Passport
 
         /// <summary>
         /// Optional. Encrypted file with the front side of the document, provided by the user. Available for
-        /// "passport", "driver_license", "identity_card" and "internal_passport". The file can be decrypted and
-        /// verified using the accompanying <see cref="EncryptedCredentials"/>.
+        /// <see cref="Passport"/>, <see cref="DriverLicence"/>, <see cref="IdentityCard"/> and
+        /// <see cref="InternalPassport"/>. The file can be decrypted and verified using the accompanying
+        /// <see cref="EncryptedCredentials"/>.
         /// </summary>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public PassportFile? FrontSide { get; set; }
 
         /// <summary>
         /// Optional. Encrypted file with the reverse side of the document, provided by the user. Available for
-        /// "driver_license" and "identity_card". The file can be decrypted and verified using the accompanying
-        /// <see cref="EncryptedCredentials"/>.
+        /// <see cref="DriverLicence"/> and <see cref="IdentityCard"/>. The file can be decrypted and verified using
+        /// the accompanying <see cref="EncryptedCredentials"/>.
         /// </summary>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public PassportFile? ReverseSide { get; set; }
 
         /// <summary>
         /// Optional. Encrypted file with the selfie of the user holding a document, provided by the user;
-        /// available for "passport", "driver_license", "identity_card" and "internal_passport". The file can
-        /// be decrypted and verified using the accompanying <see cref="EncryptedCredentials"/>.
+        /// available for <see cref="Passport"/>, <see cref="DriverLicence"/>, <see cref="IdentityCard"/> and
+        /// <see cref="InternalPassport"/>. The file can be decrypted and verified using the accompanying
+        /// <see cref="EncryptedCredentials"/>.
         /// </summary>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public PassportFile? Selfie { get; set; }
 
         /// <summary>
         /// Optional. Array of encrypted files with translated versions of documents provided by the user.
-        /// Available if requested for "passport", "driver_license", "identity_card", "internal_passport",
-        /// "utility_bill", "bank_statement", "rental_agreement", "passport_registration" and
-        /// "temporary_registration" types. Files can be decrypted and verified using the accompanying
+        /// Available if requested for <see cref="Passport"/>, <see cref="DriverLicence"/>,
+        /// <see cref="IdentityCard"/>, <see cref="InternalPassport"/>, <see cref="UtilityBill"/>,
+        /// <see cref="BankStatement"/>, <see cref="RentalAgreement"/>, <see cref="PassportRegistration"/> and
+        /// <see cref="TemporaryRegistration"/> types. Files can be decrypted and verified using the accompanying
         /// <see cref="EncryptedCredentials"/>.
         /// </summary>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
@@ -84,6 +88,6 @@ namespace Telegram.Bot.Types.Passport
         /// Base64-encoded element hash for using in PassportElementErrorUnspecified
         /// </summary>
         [JsonProperty(Required = Required.Always)]
-        public string Hash { get; set; }
+        public string Hash { get; set; } = default!;
     }
 }
---
+++ src/Telegram.Bot/Types/Passport/EncryptedPassportElementType.cs
@@ -0,0 +1,77 @@
+using Newtonsoft.Json;
+using Telegram.Bot.Converters;
+
+namespace Telegram.Bot.Types.Passport
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [JsonConverter(typeof(EncryptedPassportElementTypeConverter))]
+    public enum EncryptedPassportElementType
+    {
+        /// <summary>
+        /// Personal details
+        /// </summary>
+        PersonalDetails = 1,
+
+        /// <summary>
+        /// Passport
+        /// </summary>
+        Passport,
+
+        /// <summary>
+        /// Driver licence
+        /// </summary>
+        DriverLicence,
+
+        /// <summary>
+        /// Identity card
+        /// </summary>
+        IdentityCard,
+
+        /// <summary>
+        /// Internal passport
+        /// </summary>
+        InternalPassport,
+
+        /// <summary>
+        /// Address
+        /// </summary>
+        Address,
+
+        /// <summary>
+        /// Utility bill
+        /// </summary>
+        UtilityBill,
+
+        /// <summary>
+        /// Bank statement
+        /// </summary>
+        BankStatement,
+
+        /// <summary>
+        /// Rental agreement
+        /// </summary>
+        RentalAgreement,
+
+        /// <summary>
+        /// Passport registration
+        /// </summary>
+        PassportRegistration,
+
+        /// <summary>
+        /// Temporary registration
+        /// </summary>
+        TemporaryRegistration,
+
+        /// <summary>
+        /// Phone number
+        /// </summary>
+        PhoneNumber,
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        Email
+    }
+}
