7dc8cc1049beaaff2878a8a2b6dbbfa0d80b7d25
Aleksey Usatov <usatov.aleksey@gmail.com>
2021-07-04 16:21:07 +0300

    Update to latest client version

--- test/Telegram.Bot.Extensions.Polling.Tests/MockTelegramBotClient.cs
+++ test/Telegram.Bot.Extensions.Polling.Tests/MockTelegramBotClient.cs
@@ -8,7 +8,6 @@ using Telegram.Bot.Args;
 using Telegram.Bot.Requests;
 using Telegram.Bot.Requests.Abstractions;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 
 #pragma warning disable CS0067
 
@@ -98,22 +97,10 @@ namespace Telegram.Bot.Extensions.Polling.Tests
         // ---------------
 
         public long? BotId => throw new NotImplementedException();
-        public bool IsReceiving => throw new NotImplementedException();
-        public int MessageOffset { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public event AsyncEventHandler<ApiRequestEventArgs> OnMakingApiRequest;
         public event AsyncEventHandler<ApiResponseEventArgs> OnApiResponseReceived;
-        public event EventHandler<UpdateEventArgs> OnUpdate;
-        public event EventHandler<MessageEventArgs> OnMessage;
-        public event EventHandler<MessageEventArgs> OnMessageEdited;
-        public event EventHandler<InlineQueryEventArgs> OnInlineQuery;
-        public event EventHandler<ChosenInlineResultEventArgs> OnInlineResultChosen;
-        public event EventHandler<CallbackQueryEventArgs> OnCallbackQuery;
-        public event EventHandler<ReceiveErrorEventArgs> OnReceiveError;
-        public event EventHandler<ReceiveGeneralErrorEventArgs> OnReceiveGeneralError;
         public Task DownloadFileAsync(string filePath, Stream destination, CancellationToken cancellationToken = default) => throw new NotImplementedException();
         public Task<Types.File> GetInfoAndDownloadFileAsync(string fileId, Stream destination, CancellationToken cancellationToken = default) => throw new NotImplementedException();
-        public void StartReceiving(UpdateType[] allowedUpdates = null, CancellationToken cancellationToken = default) => throw new NotImplementedException();
-        public void StopReceiving() => throw new NotImplementedException();
         public Task<bool> TestApiAsync(CancellationToken cancellationToken = default) => throw new NotImplementedException();
     }
 }
