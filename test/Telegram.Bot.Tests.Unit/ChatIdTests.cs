3120548fcbdd4402e1fa9b2f8ed6cb79f1944bce
karb0f0s <17474471+karb0f0s@users.noreply.github.com>
2021-11-27 17:48:24 +0300

    Refactor ChatId unit tests

--- test/Telegram.Bot.Tests.Unit/ChatIdTests.cs
+++ test/Telegram.Bot.Tests.Unit/ChatIdTests.cs
@@ -8,90 +8,110 @@ namespace Telegram.Bot.Tests.Unit
 {
     public class ChatIdTests
     {
-        [Fact]
-        public void Check_Constructor()
+        [Theory]
+        [InlineData("username")]
+        [InlineData("1.23")]
+        public void Should_Throw_Exception_When_Incorrect_Identifier_Provided(string badIdentifier)
         {
-            var chatId = new ChatId(123);
+            Assert.Throws<ArgumentException>(() => new ChatId(badIdentifier));
+        }
 
-            //check int & long
+        [Theory]
+        [InlineData(123)]
+        [InlineData(123123412123341234L)]
+        public void Should_Create_ChatId_From_Value(long identifier)
+        {
+            ChatId chatId = new ChatId(identifier);
+
             Assert.Null(chatId.Username);
-            Assert.Equal(123, chatId.Identifier);
+            Assert.Equal(identifier, chatId.Identifier);
+        }
 
-            chatId = new ChatId(123L);
+        [Theory]
+        [InlineData("123", 123)]
+        [InlineData("123123412123341234", 123123412123341234L)]
+        public void Should_Create_ChatId_From_String_Value(string identifier, long identifierValue)
+        {
+            ChatId chatId = new ChatId(identifier);
+
             Assert.Null(chatId.Username);
-            Assert.Equal(123L, chatId.Identifier);
+            Assert.Equal(identifierValue, chatId.Identifier);
+        }
 
-            // check string values
-            chatId = new ChatId(123.ToString());
-            Assert.Null(chatId.Username);
-            Assert.Equal(123, chatId.Identifier);
+        [Theory]
+        [InlineData("@valid_username")]
+        public void Should_Create_ChatId_From_String(string identifier)
+        {
+            ChatId chatId = new ChatId(identifier);
 
-            chatId = new ChatId(123L.ToString());
-            Assert.Null(chatId.Username);
-            Assert.Equal(123L, chatId.Identifier);
-
-            chatId = new ChatId("@valid_username");
-            Assert.Equal("@valid_username", chatId.Username);
+            Assert.Equal(identifier, chatId.Username);
             Assert.Null(chatId.Identifier);
-
-            Assert.Throws<ArgumentException>(() => new ChatId("username"));
         }
 
-
-        [Fact]
-        public void Should_ToString()
+        [Theory]
+        [InlineData("123")]
+        [InlineData("123123412123341234")]
+        [InlineData("@valid_username")]
+        public void Should_Convert_ChatId_With_String_Value_ToString(string identifier)
         {
-            //int
-            Assert.Equal("123", new ChatId("123").ToString());
-            Assert.Equal("123", new ChatId(123).ToString());
+            ChatId chatId = new ChatId(identifier);
 
-            //long
-            Assert.Equal("123456789012", new ChatId((123456789012)).ToString());
-            Assert.Equal("123456789012", new ChatId("123456789012").ToString());
-
-
-            //username
-            Assert.Equal("@valid_username", new ChatId("@valid_username").ToString());
-
-
-            Assert.True(848993490 != new ChatId(1830775754));
+            Assert.Equal(identifier, chatId.ToString());
         }
 
-        [Fact]
-        public void Equals_Test()
+        [Theory]
+        [InlineData(123, "123")]
+        [InlineData(123123412123341234L, "123123412123341234")]
+        public void Should_Convert_ChatId_ToString(long identifier, string identifierValue)
         {
-            //with Identifier
-            var chatId = new ChatId(123);
-            Assert.True(chatId.Equals(123));
-            // ReSharper disable once SuspiciousTypeConversion.Global
-            Assert.False(123.Equals(chatId)); // to be aware
-            Assert.True(chatId == 123);
-            Assert.True(123 == chatId);
+            ChatId chatId = new ChatId(identifier);
 
-            chatId = new ChatId("123");
-            Assert.True(chatId.Equals(123));
-            // ReSharper disable once SuspiciousTypeConversion.Global
-            Assert.False(123.Equals(chatId)); // to be aware
-            Assert.True(chatId == 123);
-            Assert.True(123 == chatId);
+            Assert.Equal(identifierValue, chatId.ToString());
+        }
 
-            //with username
-            chatId = new ChatId("@username");
-            Assert.True(chatId.Equals("@username"));
-            Assert.True("@username".Equals(chatId));
-            Assert.True(chatId == "@username");
-            Assert.True("@username" == chatId);
+        [Theory]
+        [InlineData(123)]
+        [InlineData(123123412123341234L)]
+        public void Should_Be_Equal_ChatId_Identifier_To_Long_Value(long identifier)
+        {
+            ChatId chatId = new ChatId(identifier);
 
-            //with other ChatId
-            Assert.Equal(chatId, chatId);
-            Assert.Equal(new ChatId(123), new ChatId(123));
+            Assert.True(chatId.Equals(identifier));
+            Assert.False(identifier.Equals(chatId));
+            Assert.True(chatId == identifier);
+            Assert.True(identifier == chatId);
+        }
+
+        [Theory]
+        [InlineData("123", 123)]
+        [InlineData("123123412123341234", 123123412123341234L)]
+        public void Should_Be_Equal_ChatId_Identifier_To_Value(string identifier, long identifierValue)
+        {
+            ChatId chatId = new ChatId(identifier);
+
+            Assert.True(chatId.Equals(identifierValue));
+            Assert.False(identifierValue.Equals(chatId));
+            Assert.True(chatId == identifierValue);
+            Assert.True(identifierValue == chatId);
+        }
+
+        [Theory]
+        [InlineData("@valid_username")]
+        public void Should_Be_Equal_ChatId_Username_To_Identifier(string identifier)
+        {
+            ChatId chatId = new ChatId(identifier);
+
+            Assert.True(chatId.Equals(identifier));
+            Assert.True(identifier.Equals(chatId));
+            Assert.True(chatId == identifier);
+            Assert.True(identifier == chatId);
         }
 
         [Theory]
         [ClassData(typeof(ChatIdTestData))]
-        public void Equals_Test2(ChatId id1, ChatId id2, bool result)
+        public void Should_Compare_ChatId_With_Equals_Operator(ChatId id1, ChatId id2, bool result)
         {
-            Assert.Equal(id1==id2, result);
+            Assert.Equal(id1 == id2, result);
         }
     }
 
