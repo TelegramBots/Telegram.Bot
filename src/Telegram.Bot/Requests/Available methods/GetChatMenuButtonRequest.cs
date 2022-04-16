24d4f4553f63034c0a8a680bf3065a58ae59c743
Aleksey Usatov <usatov.aleksey@gmail.com>
2022-04-17 02:35:40 +0300

    Fix GetChatMenuButtonRequest request

--- src/Telegram.Bot/Requests/Available methods/GetChatMenuButtonRequest.cs
+++ src/Telegram.Bot/Requests/Available methods/GetChatMenuButtonRequest.cs
@@ -1,4 +1,5 @@
-﻿using Telegram.Bot.Requests.Abstractions;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Telegram.Bot.Types;
 
 // ReSharper disable once CheckNamespace
@@ -8,21 +9,20 @@ namespace Telegram.Bot.Requests;
 /// Use this method to get the current value of the bot`s menu button in a private chat, or the default menu button.
 /// Returns <see cref="MenuButton"/> on success.
 /// </summary>
-public class GetChatMenuButtonRequest : RequestBase<MenuButton>, IChatTargetable
+[JsonObject(MemberSerialization.OptIn, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+public class GetChatMenuButtonRequest : RequestBase<MenuButton>
 {
     /// <summary>
     /// Optional. Unique identifier for the target private chat. If not specified, default bot`s menu button
     /// will be changed
     /// </summary>
-    public long ChatId { get; }
-
-    /// <inheritdoc />
-    ChatId IChatTargetable.ChatId => ChatId;
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public long? ChatId { get; set; }
 
     /// <summary>
     /// Initializes a new request
     /// </summary>
-    public GetChatMenuButtonRequest(long chatId)
+    public GetChatMenuButtonRequest()
         : base("getChatMenuButton")
     { }
 }
