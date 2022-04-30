8eb4f3ef0ad8f222cade0a6acf92f4d63ee8bd4d
karb0f0s <17474471+karb0f0s@users.noreply.github.com>
2022-04-30 09:27:16 +0300

    Comment out affected converters

--- src/Telegram.Bot/Converters/ChatTypeConverter.cs
+++ src/Telegram.Bot/Converters/ChatTypeConverter.cs
@@ -1,37 +1,37 @@
-﻿using System.Collections.Generic;
-using Telegram.Bot.Types.Enums;
+//using System.Collections.Generic;
+//using Telegram.Bot.Types.Enums;
 
-namespace Telegram.Bot.Converters;
+//namespace Telegram.Bot.Converters;
 
-internal class ChatTypeConverter : EnumConverter<ChatType>
-{
-    static readonly IReadOnlyDictionary<string, ChatType> StringToEnum =
-        new Dictionary<string, ChatType>
-        {
-            {"private", ChatType.Private},
-            {"group", ChatType.Group},
-            {"channel", ChatType.Channel},
-            {"supergroup", ChatType.Supergroup},
-            {"sender", ChatType.Sender}
-        };
+//internal class ChatTypeConverter : EnumConverter<ChatType>
+//{
+//    static readonly IReadOnlyDictionary<string, ChatType> StringToEnum =
+//        new Dictionary<string, ChatType>
+//        {
+//            {"private", ChatType.Private},
+//            {"group", ChatType.Group},
+//            {"channel", ChatType.Channel},
+//            {"supergroup", ChatType.Supergroup},
+//            {"sender", ChatType.Sender}
+//        };
 
-    static readonly IReadOnlyDictionary<ChatType, string> EnumToString =
-        new Dictionary<ChatType, string>
-        {
-            {ChatType.Private, "private"},
-            {ChatType.Group, "group"},
-            {ChatType.Channel, "channel"},
-            {ChatType.Supergroup, "supergroup"},
-            {ChatType.Sender, "sender"}
-        };
+//    static readonly IReadOnlyDictionary<ChatType, string> EnumToString =
+//        new Dictionary<ChatType, string>
+//        {
+//            {ChatType.Private, "private"},
+//            {ChatType.Group, "group"},
+//            {ChatType.Channel, "channel"},
+//            {ChatType.Supergroup, "supergroup"},
+//            {ChatType.Sender, "sender"}
+//        };
 
-    protected override ChatType GetEnumValue(string value) =>
-        StringToEnum.TryGetValue(value, out var enumValue)
-            ? enumValue
-            : 0;
+//    protected override ChatType GetEnumValue(string value) =>
+//        StringToEnum.TryGetValue(value, out var enumValue)
+//            ? enumValue
+//            : 0;
 
-    protected override string GetStringValue(ChatType value) =>
-        EnumToString.TryGetValue(value, out var stringValue)
-            ? stringValue
-            : "unknown";
-}
+//    protected override string GetStringValue(ChatType value) =>
+//        EnumToString.TryGetValue(value, out var stringValue)
+//            ? stringValue
+//            : "unknown";
+//}
--- src/Telegram.Bot/Converters/EnumConverter.cs
+++ src/Telegram.Bot/Converters/EnumConverter.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+//using System;
+//using System.Runtime.CompilerServices;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
 
-namespace Telegram.Bot.Converters;
+//namespace Telegram.Bot.Converters;
 
-internal abstract class EnumConverter<TEnum> : JsonConverter<TEnum>
-    where TEnum : Enum
-{
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    protected abstract TEnum GetEnumValue(string value);
+//internal abstract class EnumConverter<TEnum> : JsonConverter<TEnum>
+//    where TEnum : Enum
+//{
+//    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+//    protected abstract TEnum GetEnumValue(string value);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    protected abstract string GetStringValue(TEnum value);
+//    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+//    protected abstract string GetStringValue(TEnum value);
 
-    public override void WriteJson(JsonWriter writer, TEnum value, JsonSerializer serializer) =>
-        writer.WriteValue(GetStringValue(value));
+//    public override void WriteJson(JsonWriter writer, TEnum value, JsonSerializer serializer) =>
+//        writer.WriteValue(GetStringValue(value));
 
-    public override TEnum ReadJson(
-        JsonReader reader,
-        Type objectType,
-        TEnum existingValue,
-        bool hasExistingValue,
-        JsonSerializer serializer
-    ) =>
-        GetEnumValue(JToken.ReadFrom(reader).Value<string>());
-}
+//    public override TEnum ReadJson(
+//        JsonReader reader,
+//        Type objectType,
+//        TEnum existingValue,
+//        bool hasExistingValue,
+//        JsonSerializer serializer
+//    ) =>
+//        GetEnumValue(JToken.ReadFrom(reader).Value<string>());
+//}
