51cfe485a253de829ab6f2b1c3398108ee6e5bf1
karb0f0s <17474471+karb0f0s@users.noreply.github.com>
2023-04-24 12:25:27 +0300

    Add WebAppName to WriteAccessAllowed

--- src/Telegram.Bot/Types/WriteAccessAllowed.cs
+++ src/Telegram.Bot/Types/WriteAccessAllowed.cs
@@ -1,8 +1,15 @@
 namespace Telegram.Bot.Types;
 
 /// <summary>
-/// This object represents a service message about a user allowing a bot added to the attachment menu to write
-/// messages. Currently holds no information.
+/// This object represents a service message about a user allowing a bot to write messages
+/// after adding the bot to the attachment menu or launching a Web App from a link.
 /// </summary>
 [JsonObject(MemberSerialization.OptIn, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
-public class WriteAccessAllowed { }
+public class WriteAccessAllowed
+{
+    /// <summary>
+    /// Optional. Name of the Web App which was launched from a link
+    /// </summary>
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public string? WebAppName { get; set; }
+}
