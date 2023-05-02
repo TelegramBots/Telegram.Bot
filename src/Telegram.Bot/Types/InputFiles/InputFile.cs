0608c7d46108614395675f953e10bc2f020ac306
Aleksey Usatov <usatov.aleksey@gmail.com>
2023-05-02 21:19:15 +0400

    Add XML docs

--- src/Telegram.Bot/Types/InputFiles/InputFile.cs
+++ src/Telegram.Bot/Types/InputFiles/InputFile.cs
@@ -20,35 +20,35 @@ public abstract class InputFile
     public abstract FileType FileType { get; }
 
     /// <summary>
-    /// Creates a correct <see cref="InputFile"/> object
+    /// Creates an instance of <see cref="InputFile"/> from a string containing a file's URL or file id
     /// </summary>
-    /// <param name="urlOrFileId">A URL or a file id</param>
-    /// <returns></returns>
+    /// <param name="urlOrFileId">A file's URL or a file id</param>
+    /// <returns>An instance of a class that implements <see cref="InputFile"/></returns>
     public static InputFile FromString(string urlOrFileId) =>
         Uri.TryCreate(urlOrFileId, UriKind.Absolute, out var url)
             ? new InputFileUrl(url)
             : new InputFileId(urlOrFileId);
 
     /// <summary>
-    ///
+    /// Creates an <see cref="InputFileStream"/> from an instance <see cref="Stream"/>
     /// </summary>
-    /// <param name="stream"></param>
-    /// <param name="fileName"></param>
-    /// <returns></returns>
+    /// <param name="stream">A <see cref="Stream"/> with file data to upload</param>
+    /// <param name="fileName">An optional file name</param>
+    /// <returns>An instance of <see cref="InputFileStream"/></returns>
     public static InputFileStream FromStream(Stream stream, string? fileName = default) =>
         new(stream.ThrowIfNull(), fileName);
 
     /// <summary>
-    ///
+    /// Creates an <see cref="InputFileUrl"/> from an instance <see cref="Uri"/>
     /// </summary>
     /// <param name="url"></param>
-    /// <returns></returns>
+    /// <returns>An instance of <see cref="InputFileUrl"/></returns>
     public static InputFileUrl FromUri(Uri url) => new(url.ThrowIfNull());
 
     /// <summary>
-    ///
+    /// Creates an <see cref="InputFileId"/> from a file id
     /// </summary>
-    /// <param name="fileId"></param>
-    /// <returns></returns>
+    /// <param name="fileId">An ID of a file</param>
+    /// <returns>An instance of <see cref="InputFileId"/></returns>
     public static InputFileId FromFileId(string fileId) => new(fileId.ThrowIfNull());
 }
