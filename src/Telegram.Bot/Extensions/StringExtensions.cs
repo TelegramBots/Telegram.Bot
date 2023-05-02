7b30bfeaa01fc5433db71a9d5dc8d24d7ae9cadb
Aleksey Usatov <usatov.aleksey@gmail.com>
2023-05-02 21:18:25 +0400

    Remove unnecessary class extension

--- src/Telegram.Bot/Extensions/StringExtensions.cs
+++ src/Telegram.Bot/Extensions/StringExtensions.cs
@@ -9,12 +9,4 @@ internal static class StringExtensions
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static string EncodeUtf8(this string value) =>
         new(Encoding.UTF8.GetBytes(value).Select(c => Convert.ToChar(c)).ToArray());
-
-    /// <summary>
-    /// Converts a string to a correct <see cref="InputFile"/> implementation depending
-    /// if it is a well formed URL or not
-    /// </summary>
-    /// <param name="urlOrFileId">A string with a URL of a file or a file id</param>
-    /// <returns>A correct implementation of <see cref="InputFile"/></returns>
-    public static InputFile ToInputFile(this string urlOrFileId) => InputFile.FromString(urlOrFileId);
 }
