b8afccb94b426084f0b12bde2162a2f758e45147
karb0f0s <17474471+karb0f0s@users.noreply.github.com>
2023-08-21 12:04:43 +0300

    Add Story property to the Message class

---
+++ src/Telegram.Bot/Types/Story.cs
@@ -0,0 +1,9 @@
+namespace Telegram.Bot.Types;
+
+/// <summary>
+/// This object represents a message about a forwarded story in the chat. Currently holds no information.
+/// </summary>
+[JsonObject(MemberSerialization.OptIn, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+public class Story
+{
+}
