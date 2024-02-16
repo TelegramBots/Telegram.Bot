2c45e70323bec574f3d1a1e577788a1bd2aaa78e
Aleksey Usatov <usatov.aleksey@gmail.com>
2024-02-17 00:58:40 +0400

    Add unit tests for Chat and ChatBoostAdded

---
+++ test/Telegram.Bot.Tests.Unit/Serialization/ChatBoostAddedSerializationTests.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Telegram.Bot.Types;
+using Xunit;
+
+namespace Telegram.Bot.Tests.Unit.Serialization;
+
+public class ChatBoostAddedSerializationTests
+{
+    [Fact]
+    public void Should_Deserialize_ChatBoostAdded()
+    {
+        const string chatBoostAdded =
+            """
+            {
+              "boost_count": 101
+            }
+            """;
+
+        ChatBoostAdded? deserialize = JsonConvert.DeserializeObject<ChatBoostAdded>(chatBoostAdded);
+
+        Assert.NotNull(deserialize);
+        Assert.Equal(101, deserialize.BoostCount);
+    }
+
+    [Fact]
+    public void Should_Serialize_ChatBoostAdded()
+    {
+        ChatBoostAdded chat = new()
+        {
+            BoostCount = 101,
+        };
+
+        ChatBoostAdded? deserialized = JsonConvert.DeserializeObject<ChatBoostAdded>(JsonConvert.SerializeObject(chat));
+
+        Assert.NotNull(deserialized);
+        Assert.Equal(deserialized.BoostCount, deserialized.BoostCount);
+    }
+}
--- test/Telegram.Bot.Tests.Unit/Serialization/ChatSerializationTests.cs
+++ test/Telegram.Bot.Tests.Unit/Serialization/ChatSerializationTests.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using Xunit;
 
 namespace Telegram.Bot.Tests.Unit.Serialization;
@@ -9,15 +10,38 @@ public class ChatSerializationTests
     [Fact]
     public void Should_Deserialize_Chat()
     {
+        const string chat =
+            """
+            {
+              "id": 12345,
+              "type": "supergroup",
+              "unrestrict_boost_count": 10,
+              "custom_emoji_sticker_set_name": "test_sticker_set"
+            }
+            """;
+
+        Chat? deserialize = JsonConvert.DeserializeObject<Chat>(chat);
+
+        Assert.NotNull(deserialize);
+        Assert.Equal(10, deserialize.UnrestrictBoostCount);
+        Assert.Equal(12345, deserialize.Id);
+        Assert.Equal(ChatType.Supergroup, deserialize.Type);
+        Assert.Equal("test_sticker_set", deserialize.CustomEmojiStickerSetName);
+    }
+
+    [Fact]
+    public void Should_Serialize_Chat()
+    {
         Chat chat = new()
         {
             UnrestrictBoostCount = 10,
             CustomEmojiStickerSetName = "test_sticker_set"
         };
 
-        string serializeChat = JsonConvert.SerializeObject(chat);
+        Chat? deserialized = JsonConvert.DeserializeObject<Chat>(JsonConvert.SerializeObject(chat));
 
-        Assert.Contains(@"""unrestrict_boost_count"":10", serializeChat);
-        Assert.Contains(@"""custom_emoji_sticker_set_name"":""test_sticker_set""", serializeChat);
+        Assert.NotNull(deserialized);
+        Assert.Equal(deserialized.UnrestrictBoostCount, deserialized.UnrestrictBoostCount);
+        Assert.Equal(chat.CustomEmojiStickerSetName, deserialized.CustomEmojiStickerSetName);
     }
 }
