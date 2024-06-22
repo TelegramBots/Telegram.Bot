4dff8dae62a09db2fe9cf0e31ee83ec2b9dd5611
Wizou <11647984+wiz0u@users.noreply.github.com>
2024-06-22 15:04:18 +0200

    even simpler ConfigureTelegramBot helper

--- src/Telegram.Bot/Extensions/Extensions.cs
+++ src/Telegram.Bot/Extensions/Extensions.cs
@@ -1,16 +1,38 @@
 using System.Runtime.CompilerServices;
 
-namespace Telegram.Bot.Extensions;
-
-/// <summary>
-/// Extension Methods
-/// </summary>
-internal static class ObjectExtensions
+namespace Telegram.Bot.Extensions
 {
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static T ThrowIfNull<T>(
-        this T? value,
-        [CallerArgumentExpression(nameof(value))] string? parameterName = default
-    ) =>
-        value ?? throw new ArgumentNullException(parameterName);
+    /// <summary>
+    /// Extension Methods
+    /// </summary>
+    internal static class ObjectExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static T ThrowIfNull<T>(
+            this T? value,
+            [CallerArgumentExpression(nameof(value))] string? parameterName = default
+        ) =>
+            value ?? throw new ArgumentNullException(parameterName);
+    }
+}
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Helpers for API service configuration
+    /// </summary>
+    public static class TelegramBotConfigureExtensions
+    {
+        /// <summary>
+        /// Configure JsonOptions for Telegram.Bot (de)serialization
+        /// </summary>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the services to.</param>
+        /// <param name="opt">Accessor to JsonSerializerOptions</param>
+        /// <returns></returns>
+        public static IServiceCollection ConfigureTelegramBot<TOptions>(this IServiceCollection services, Func<TOptions, JsonSerializerOptions> opt)
+            where TOptions : class
+        {
+            return services.Configure<TOptions>(options => JsonSerializerOptionsProvider.Configure(opt(options)));
+        }
+    }
 }
