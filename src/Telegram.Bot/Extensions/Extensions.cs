fbca5e3afd89fe8f14f73175db9293a359f6d660
Wizou <11647984+wiz0u@users.noreply.github.com>
2024-06-27 15:40:11 +0200

    Support for MvcOptions with specific STJ formatter for Update/IRequest only

--- src/Telegram.Bot/Extensions/Extensions.cs
+++ src/Telegram.Bot/Extensions/Extensions.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Telegram.Bot.Extensions
 {
@@ -18,21 +22,60 @@ namespace Telegram.Bot.Extensions
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    /// <summary>
-    /// Helpers for API service configuration
-    /// </summary>
+    /// <summary>Helpers for WebApp service configuration</summary>
     public static class TelegramBotConfigureExtensions
     {
-        /// <summary>
-        /// Configure JsonOptions for Telegram.Bot (de)serialization
-        /// </summary>
-        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the services to.</param>
+        /// <summary>Configure WebAPI JsonOptions for Telegram.Bot (de)serialization</summary>
+        /// <param name="services">The IServiceCollection to add the services to.</param>
         /// <param name="opt">Accessor to JsonSerializerOptions</param>
-        /// <returns></returns>
         public static IServiceCollection ConfigureTelegramBot<TOptions>(this IServiceCollection services, Func<TOptions, JsonSerializerOptions> opt)
             where TOptions : class
+            => services.Configure<TOptions>(options => JsonSerializerOptionsProvider.Configure(opt(options)));
+
+        /// <summary>Configure ASP.NET MVC Json (de)serialization for Telegram.Bot types</summary>
+        /// <param name="services">The IServiceCollection to add the services to.</param>
+        public static IServiceCollection ConfigureTelegramBotMvc(this IServiceCollection services)
+            => services.Configure<MvcOptions>(options =>
+            {
+                options.InputFormatters.Insert(0, InputFormatter);
+                options.OutputFormatters.Insert(0, OutputFormatter);
+            });
+
+        private static readonly TelegramBotOutputFormatter OutputFormatter = new();
+        private static readonly TelegramBotInputFormatter InputFormatter = new();
+    }
+
+    class TelegramBotInputFormatter : TextInputFormatter
+    {
+        public TelegramBotInputFormatter()
         {
-            return services.Configure<TOptions>(options => JsonSerializerOptionsProvider.Configure(opt(options)));
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedMediaTypes.Add("application/json");
+        }
+
+        protected override bool CanReadType(Type type) => type == typeof(Update);
+
+        public sealed override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+        {
+            var model = await JsonSerializer.DeserializeAsync(context.HttpContext.Request.Body, context.ModelType, JsonSerializerOptionsProvider.Options);
+            return InputFormatterResult.Success(model);
+        }
+    }
+
+    class TelegramBotOutputFormatter : TextOutputFormatter
+    {
+        public TelegramBotOutputFormatter()
+        {
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedMediaTypes.Add("application/json");
+        }
+
+        protected override bool CanWriteType(Type type) => typeof(IRequest).IsAssignableFrom(type);
+
+        public sealed override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var stream = context.HttpContext.Response.Body;
+            await JsonSerializer.SerializeAsync(stream, context.Object, JsonSerializerOptionsProvider.Options, context.HttpContext.RequestAborted);
         }
     }
 }
--- src/Telegram.Bot/Serialization/JsonSerializerOptionsProvider.cs
+++ src/Telegram.Bot/Serialization/JsonSerializerOptionsProvider.cs
@@ -1,38 +1,26 @@
-using System.Collections.Generic;
-
 namespace Telegram.Bot.Serialization;
 
-/// <summary>
-///
-/// </summary>
+/// <summary>Static class offering JsonSerializerOptions configured for Telegram.Bot serialization</summary>
 public static partial class JsonSerializerOptionsProvider
 {
-    static JsonSerializerOptionsProvider()
-    {
-        Options = new JsonSerializerOptions();
-        Configure(Options);
-        AddGeneratedConverters(Options.Converters);
-    }
-
-    /// <summary>
-    ///
-    /// </summary>
+    /// <summary>JsonSerializerOptions configured for Telegram.Bot serialization</summary>
     public static JsonSerializerOptions Options { get; }
 
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name="options"></param>
+    static JsonSerializerOptionsProvider() => Configure(Options = new JsonSerializerOptions());
+
+    /// <summary>Configure JsonSerializerOptions for Telegram.Bot serialization</summary>
+    /// <param name="options">JsonSerializerOptions to configure</param>
     public static void Configure(JsonSerializerOptions options)
     {
-        options.Converters.Add(new UnixDateTimeConverter());
-        options.Converters.Add(new BanTimeConverter());
-        options.Converters.Add(new InputFileConverter());
-        options.Converters.Add(new ChatIdConverter());
-        options.Converters.Add(new PolymorphicJsonConverterFactory());
         options.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
         options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
         options.UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip;
+        options.Converters.Add(new BanTimeConverter());
+        options.Converters.Add(new ChatIdConverter());
+        options.Converters.Add(new InputFileConverter());
+        options.Converters.Add(new PolymorphicJsonConverterFactory());
+        options.Converters.Add(new UnixDateTimeConverter());
+        AddGeneratedConverters(Options.Converters);
     }
 
     static partial void AddGeneratedConverters(IList<JsonConverter> converters);
